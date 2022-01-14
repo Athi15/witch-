8200852f1bf70274e904a6184cd900459783e65a
Athi15 <97509009+Athi15@users.noreply.github.com>
2022-01-14 10:17:00 +0530

    Condition statement
    This file print choice other than 1,0

---
+++ taskno2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int x = 2;
+  switch(x)
+  {
+  	case 1:printf("choice is 0\n");
+  	case 2:printf("choice is 1\n");
+  	   break;
+  	default:printf("choice other than 0,1");
+  }
+  return 0;	
+}
